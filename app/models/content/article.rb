21dc2307c9975416d6df4172acaca459bcf15f82
eelco <git@eelcoj.com>
2025-09-28 10:43:55 +0700

    Root restructure + docs set up

--- app/models/content/article.rb
+++ app/models/content/article.rb
@@ -1,5 +1,2 @@
 class Content::Article < Perron::Resource
-  configure do |config|
-    config.metadata.type = "article"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles, path: "docs", module: :content, only: %w[index show]
   resources :pages, path: "/", module: :content, only: %w[show], constraints: { id: /[^\/]+/ }
 
   root to: "content/pages#root"
