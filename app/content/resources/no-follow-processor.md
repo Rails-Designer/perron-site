c72a6d2878005c746d9eaa2df56505387f84344a
eelco <git@eelcoj.com>
2025-10-08 11:03:10 +0700

    Added new rel=nofollow html processor

---
+++ app/content/resources/no-follow-processor.md
@@ -0,0 +1,7 @@
+---
+type: snippet
+name: Processor to add `rel=nofollow`
+description: This snippet adds a processor for the `markdownify` helper to add `rel=nofollow` to your outgoing links.
+---
+
+This snippet adds a processor for the `markdownify` helper to add `rel=nofollow` to your outgoing links, it skips internal links—those starting with a `/` and `#` (anchors).
--- app/processors/nofollow_processor.rb
+++ app/processors/nofollow_processor.rb
@@ -1,11 +1,20 @@
 class NofollowProcessor < Perron::HtmlProcessor::Base
   def process
     @html.css("a").each do |link|
-      href = link["href"]
-
-      next if href.blank? || href.start_with?("/", "#", "mailto:")
+      next if skippable? link
 
       link["rel"] = "nofollow"
     end
+  end
+
+  private
+
+  def skippable?(link)
+    href = link["href"]
+
+    href.blank? ||
+      href.start_with?("https://perron.railsdesigner.com") ||
+      href.start_with?("/", "#", "mailto:") ||
+      link["rel"].present?
   end
 end
