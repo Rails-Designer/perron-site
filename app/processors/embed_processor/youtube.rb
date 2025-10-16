228aeeb6379eb5a421d62240f81e1a3d1737bd08
eelco <git@eelcoj.com>
2025-10-16 11:51:36 +0700

    Added option to youtube-nocookie.com in the embed class
    Should this option be the default? 🤔

--- app/processors/embed_processor/youtube.rb
+++ app/processors/embed_processor/youtube.rb
@@ -9,6 +9,8 @@ class EmbedProcessor
 
       Nokogiri::XML::Node.new("iframe", html).tap do |iframe|
         iframe["src"] = "https://www.youtube.com/embed/#{id}"
+        # Use this line to stop YouTube tracking your visitors who don't play the video
+        # iframe["src"] = "https://www.youtube-nocookie.com/embed/#{id}"
         iframe["width"] = "100%"
         iframe["height"] = "400"
         iframe["allowfullscreen"] = "true"
